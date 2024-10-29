ceeb9dc9c3e94831f106b1dd08ed6bafccf0b065
arach chenari <arashchenari@yahoo.com>
2024-10-30 02:32:01 +0400

    Add fluent migrator and create medication migration

--- src/MedicationSystem.Entities/Medication.cs
+++ src/MedicationSystem.Entities/Medication.cs
@@ -1,4 +1,5 @@
 using MedicationSystem.Contracts.Interfaces;
+using MedicationSystem.Entities.Enums;
 
 namespace MedicationSystem.Entities
 {
@@ -7,7 +8,7 @@ namespace MedicationSystem.Entities
         public int Id { get; set; }
         public string Code { get; set; }
         public string Name { get; set; }
-        public string Type { get; set; }
+        public MedicationType Type { get; set; }
         public int Quantity { get; set; }
     }
 }
